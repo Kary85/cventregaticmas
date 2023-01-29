c568729296892d1b7922b4ac9eaf777f2d0f83f4
Kary85 <123891998+Kary85@users.noreply.github.com>
2023-01-29 14:15:30 -0300

    Add files via upload

---
+++ CV Definitivo/JS/funtion.js
@@ -0,0 +1,4 @@
+document.getElementById('boton').onclick = function () {
+    console.log ("evento click en Nro de celular");
+    document.getElementById("boton").innerHTML = "Cel: (587) 399-3947"
+}
